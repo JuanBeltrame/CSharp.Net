95185a9f84ec8b5a6e599220e5acfdfef14ff222
Juan Beltrame <juan_beltrame@hotmail.com>
2022-07-25 16:46:57 -0300

    Update Codigo

---
+++ C#.UTN/Entidades/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entidades
+{
+    public class Estudiante : Persona
+    {
+        public int legajo;
+        public List<string> materias;
+
+        public Estudiante (int legajo, List<string> materias, long dni, string nomnbre, string apellido):this(new Persona((nombre,apellido,dni),legajo)
+        {
+            
+        }
+
+        public Estudiante (Persona persona, int legajo):base(persona.apellido, persona.nombre, persona.dni)
+        {
+            this.legajo = legajo;
+            this.materias = materias;
+        }
+    }
+}
