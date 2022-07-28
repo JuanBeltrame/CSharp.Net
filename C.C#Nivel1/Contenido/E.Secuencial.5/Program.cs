27aeb4637c038f0b11f10556a1dd6e7adeca436e
Juan Beltrame <juan_beltrame@hotmail.com>
2022-07-28 12:33:58 -0300

    .

---
+++ C.C#Nivel1/Contenido/E.Secuencial.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E.Secuencial._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
